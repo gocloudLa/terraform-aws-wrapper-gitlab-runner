d3419a83da12e2d866bb29de4894e0f8988f4b4a
Federico Midaglia <fmidaglia@gocloud.la>
2025-09-04 14:31:51 -0300

    feat(module): metadata refactor

--- examples/complete/locals.tf
+++ examples/complete/locals.tf
@@ -1,31 +1,5 @@
 locals {
 
-  metadata = {
-    aws_region  = "us-east-1"
-    environment = "Production"
-
-    public_domain  = "democorp.cloud"
-    private_domain = "democorp"
-
-    key = {
-      company = "dmc"
-      region  = "use1"
-      env     = "prd"
-    }
-  }
-
-  common_name = join("-", [
-    local.metadata.key.company,
-    local.metadata.key.env
-  ])
-
-  common_tags = {
-    "company"     = local.metadata.key.company
-    "provisioner" = "terraform"
-    "environment" = local.metadata.environment
-    "created-by"  = "GoCloud.la"
-  }
-
   asg_schedules = {
     scale_in_recurrence  = "0 01 * * *"
     scale_in_count       = 0
--- locals.tf
+++ locals.tf
@@ -1,20 +1,3 @@
 locals {
-  metadata = var.metadata
-
-  common_name = join("-", [
-    local.metadata.key.company,
-    local.metadata.key.env
-  ])
-
-  common_tags = {
-    "company"     = local.metadata.key.company
-    "provisioner" = "terraform"
-    "environment" = local.metadata.environment
-    "created-by"  = "GoCloud.la"
-  }
-
   gitlab_runner_enable = lookup(var.gitlab_runner_parameters, "enable", false) ? 1 : 0
-  default_vpc_name     = local.common_name
-  default_subnet_name  = "${local.common_name}-private*"
-
 }
