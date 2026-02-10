876446a9b71b72092c92b52ff67755cd8ab93da9
Federico Midaglia <fmidaglia@gocloud.la>
2026-02-10 14:02:41 -0300

    feat(sg): add custom rules support (#13)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -1,3 +1,20 @@
 locals {
   gitlab_runner_enable = lookup(var.gitlab_runner_parameters, "enable", false) ? 1 : 0
+
+  default_runner_egress_rules = {
+    "allow_https_ipv4" = {
+      cidr_block  = "0.0.0.0/0"
+      description = "Allow HTTPS egress traffic"
+      from_port   = 443
+      protocol    = "tcp"
+      to_port     = 443
+    }
+    "allow_https_ipv6" = {
+      description     = "Allow HTTPS egress traffic (IPv6)"
+      from_port       = 443
+      ipv6_cidr_block = "::/0"
+      protocol        = "tcp"
+      to_port         = 443
+    }
+  }
 }
